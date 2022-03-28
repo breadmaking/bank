c7ad4544e8431943761f2c9ee99aee1121213e3d
breadmaking <liquidgoldlabel@gmail.com>
2022-03-28 11:23:52 +0100

    completed bank

---
+++ bank/bank.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from multiprocessing.dummy import current_process
+
+
+class NoBalanceAvailable(Exception):
+    def __init__(self, curent_balance, withdraw_amount):
+        self.error = f"Your balance is {curent_balance}. You do not have enough to withdraw {withdraw_amount}."
+        super().__init__(self.error)
+
+
+class Account:
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.actions = []
+
+    def deposit(self, deposit_amount):
+        self.balance += deposit_amount
+        self.actions.append(
+            {
+                "type": "deposit",
+                "amount": deposit_amount,
+                "date": datetime.now(),
+                "current_balance": self.balance,
+            }
+        )
+
+    def withdraw(self, withdraw_amount):
+        if withdraw_amount > self.balance:
+            raise NoBalanceAvailable(self.balance, withdraw_amount)
+        else:
+            self.balance -= withdraw_amount
+            self.actions.append(
+                {
+                    "type": "withdraw",
+                    "amount": withdraw_amount,
+                    "date": datetime.now(),
+                    "current_balance": self.balance,
+                }
+            )
+
+    def print_statement(self):
+        str_list = ["date || credit || debit || balance"]
+        self.actions.sort(key=lambda item: item["date"], reverse=True)
+        for action in self.actions:
+            if action["type"] == "deposit":
+                str = f"{action['date'].strftime('%d/%m/%Y')} || {'{0:.2f}'.format(action['amount'])} || || {'{0:.2f}'.format(action['current_balance'])}"
+                str_list.append(str)
+            else:
+                str = f"{action['date'].strftime('%d/%m/%Y')} || || {'{0:.2f}'.format(action['amount'])} || {'{0:.2f}'.format(action['current_balance'])}"
+                str_list.append(str)
+
+        return print("\n".join(str_list))
---
+++ tests/test_bank.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from unittest.mock import patch, Mock
+import pytest
+from freezegun import freeze_time
+from bank.bank import Account, NoBalanceAvailable
+
+
+@pytest.fixture(autouse=True)
+def test_account():
+    account = Account()
+    return account
+
+
+def test_bank_account_has_balance(test_account):
+    assert test_account.balance == 0
+
+
+def test_bank_account_balance_increases_on_deposit(test_account):
+    test_account.deposit(1000)
+    assert test_account.balance == 1000
+
+
+def test_bank_account_balance_decreases_on_withdraw(test_account):
+    test_account.deposit(1000)
+    test_account.withdraw(500)
+    assert test_account.balance == 500
+
+
+def test_bank_account_stores_an_action(test_account):
+    assert test_account.actions == []
+
+
+@freeze_time("2022-03-27")
+def test_bank_account_stores_a_deposit(test_account):
+    test_account.deposit(1000)
+    assert test_account.actions == [
+        {"type": "deposit", "amount": 1000, "date": datetime(2022, 3, 27), "current_balance": 1000}
+    ]
+
+
+@freeze_time("2022-03-27")
+def test_bank_account_stores_multiple_deposits(test_account):
+    test_account.deposit(1000)
+    test_account.deposit(500)
+    assert test_account.actions == [
+        {"type": "deposit", "amount": 1000, "date": datetime(2022, 3, 27), "current_balance": 1000},
+        {"type": "deposit", "amount": 500, "date": datetime(2022, 3, 27), "current_balance": 1500},
+    ]
+
+
+@freeze_time("2022-03-27")
+def test_bank_account_stores_a_withdraw(test_account):
+    test_account.deposit(1000)
+    test_account.withdraw(1000)
+    assert test_account.actions == [
+        {"type": "deposit", "amount": 1000, "date": datetime(2022, 3, 27), "current_balance": 1000},
+        {"type": "withdraw", "amount": 1000, "date": datetime(2022, 3, 27), "current_balance": 0},
+    ]
+
+
+@freeze_time("2022-03-27")
+def test_bank_account_stores_multiple_withdraws(test_account):
+    test_account.deposit(1000)
+    test_account.withdraw(500)
+    test_account.withdraw(250)
+    assert test_account.actions == [
+        {"type": "deposit", "amount": 1000, "date": datetime(2022, 3, 27), "current_balance": 1000},
+        {"type": "withdraw", "amount": 500, "date": datetime(2022, 3, 27), "current_balance": 500},
+        {"type": "withdraw", "amount": 250, "date": datetime(2022, 3, 27), "current_balance": 250},
+    ]
+
+
+def test_withdraw_cannot_exceed_balance_total(test_account):
+    with pytest.raises(NoBalanceAvailable) as e:
+        test_account.withdraw(500)
+        assert (
+            str(e.value)
+            == f"Your balance is 0. You do not have enough to withdraw 500."
+        )
+
+@patch("bank.bank.datetime")
+def test_account_can_print_statement(datetime_mock, test_account, capsys):
+    datetime_mock.now.return_value = datetime(2023, 1, 10)
+    test_account.deposit(1000)
+    datetime_mock.now.return_value = datetime(2023, 1, 13)
+    test_account.deposit(2000)
+    datetime_mock.now.return_value = datetime(2023, 1, 14)
+    test_account.withdraw(500)
+    test_account.print_statement()
+    out, err = capsys.readouterr()
+    print(out)
+    assert out == "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00\n"
+    assert err == ''
